310feab0823ed6d4f1c9a14c98f74403b339e118
glebik000 <glebik000@gmail.com>
2020-05-09 00:28:58 +1000

    It's my test commit message:  Here was added list of weekdays and time of lessons, to create the abillity of integrating autoAFK.

--- getLogin.py
+++ getLogin.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-
+import math
+import random
 
 logging_succes = False
 USERNAME = ''
@@ -18,6 +19,8 @@ ARR_ID_COURSE = ['13509', '14498', '14643', '14506', '14425', '14534']
 
 URL_TO_COURSE = 'https://edu.vvsu.ru/course/view.php?id='
 url_to_course = URL_TO_COURSE
+
+localtime = time.localtime(time.time())
 
 
 browser = webdriver.Firefox()
@@ -54,18 +57,144 @@ if browser.find_element_by_name('btnlogin'):
 
 logging_succes = log_in(USERNAME, PASSWORD)
 
+def getCurWeekday():
+    times = time.localtime(time.time())
+    curweek = times.tm_wday
+    return curweek
+
+def getCurTimeLesson():
+    times = time.localtime(time.time())
+    curHour = times.tm_hour
+    curMinute = times.tm_min
+    curLesson = 0
+    if curHour == 8 and curMinute >= 30 or curHour == 9:
+        curLesson = 0
+        print("Now lesson - ", (curLesson+1))
+        return curLesson
+    if curHour == 10 and curMinute >= 5 or curHour == 11 and curMinute <= 40:
+        curLesson = 1
+        print("Now lesson - ", (curLesson + 1))
+        return curLesson
+    if curHour == 11 and curMinute >= 45 or curHour == 12 or curHour == 13 and curMinute <=20:
+        curLesson = 2
+        print("Now lesson - ", (curLesson + 1))
+        return curLesson
+    if curHour == 13 and curMinute >= 25 or curHour == 14 or curHour == 15 and curMinute <= 3:
+        curLesson = 3
+        print("Now lesson - ", (curLesson + 1))
+        return curLesson
+    if curHour == 15 and curMinute >= 5 or curHour == 16 and curMinute <= 40:
+        curLesson = 4
+        print("Now lesson - ", (curLesson + 1))
+        return curLesson
+    if curHour == 16 and curMinute >= 45 or curHour == 17 or curHour == 18 and curMinute <= 20:
+        curLesson = 5
+        print("Now lesson - ", (curLesson + 1))
+        return curLesson
+
+def getCurrentCourse:
+    curWeekday = getCurWeekday()
+    curLesson = getCurTimeLesson()
+    if curWeekday == 0:
+        if curLesson == 0:
+            print()
+        if curLesson == 1:
+            print()
+        if curLesson == 2:
+            print()
+        if curLesson == 3:
+            print()
+        if curLesson == 4:
+            print()
+        if curLesson == 5:
+            print()
+
+    if curWeekday == 1:
+        if curLesson == 0:
+            print()
+        if curLesson == 1:
+            print()
+        if curLesson == 2:
+            print()
+        if curLesson == 3:
+            print()
+        if curLesson == 4:
+            print()
+        if curLesson == 5:
+            print()
+
+    if curWeekday == 2:
+        if curLesson == 0:
+            print()
+        if curLesson == 1:
+            print()
+        if curLesson == 2:
+            print()
+        if curLesson == 3:
+            print()
+        if curLesson == 4:
+            print()
+        if curLesson == 5:
+            print()
+
+    if curWeekday == 3:
+        if curLesson == 0:
+            print()
+        if curLesson == 1:
+            print()
+        if curLesson == 2:
+            print()
+        if curLesson == 3:
+            print()
+        if curLesson == 4:
+            print()
+        if curLesson == 5:
+            print()
+
+    if curWeekday == 4:
+        if curLesson == 0:
+            print()
+        if curLesson == 1:
+            print()
+        if curLesson == 2:
+            print()
+        if curLesson == 3:
+            print()
+        if curLesson == 4:
+            print()
+        if curLesson == 5:
+            print()
+
+    if curWeekday == 5:
+        if curLesson == 0:
+            print()
+        if curLesson == 1:
+            print()
+        if curLesson == 2:
+            print()
+        if curLesson == 3:
+            print()
+        if curLesson == 4:
+            print()
+        if curLesson == 5:
+            print()
+
+
 what_course = 0
+
 if logging_succes:
     print("Logging succes, pls choose course:<1-6>")
 
     what_course = input()
     time.sleep(2)
-    if what_course !=0:
-        select_course(what_course)
+    if what_course != 0:
+        select_course(what_course-1)
 print("Your chose: ", what_course)
 
 while True:
+    sleep_time = 180 + (random.random()*360)
     time.sleep(360)
+    print("Refreshing...")
     browser.refresh()
 
 
